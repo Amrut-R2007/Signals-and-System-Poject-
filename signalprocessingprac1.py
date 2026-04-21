d095577793d973a8e4d7c40f3b3545d68c80481c
Amrut.R <18750amrut@gmail.com>
2026-04-21 22:06:05 +0530

    Add files via upload
    This python script Converts time domain signals to frequency domain using fast Fourier transform .

---
+++ signalprocessingprac1.py
@@ -0,0 +1,18 @@
+import librosa
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt 
+
+Y,sr=librosa.load(r"C:\Users\Amrut\Python\PythonPrograms\sp01_exhibition_sn10.wav",sr=None)
+
+Y=np.fft.fft(Y)
+frequency=np.fft.fftfreq(len(Y),d=1/sr )
+half=len(Y)//2
+Y=np.abs(Y[:half])
+frequency=frequency[:half]
+plt.figure()
+plt.plot(frequency,Y)
+plt.xlabel ("Frequency")
+plt.ylabel(" Amplitude ")
+plt.title("FFT")
+plt.show()
